321b691b7a7dd8806771dd1771ccf5ec9dd84b77
marcosviniciius <marcosv.corp@gmail.com>
2025-03-25 01:38:46 +0000

    Criei carta 1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+int main(){
+    printf("Super Trunfo - Países\n");
+   printf("Cartas 1 e 2\n");
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+//CARTA 1 - OSASCO
 
+    char Estado = 'A';
+    char codigo_da_carta[4] = "A01";
+    char cidade[30] = "Osasco";
+    int populacao = 720000;
+    float kmq = 64.954;
+    float pib = 86.1;
+    int pt = 15;
+
+// CARTA 2 - SÃO FÉLIX DO XINGU
+
+    char estado = 'H';
+    char codigodacarta[4] = "H03";
+    char cidade2[30] = "São Félix Do Xingu";
+    int populacao2 = 65000;
+    float kmq2 = 84.212;
+    float pib2 = 2.009;
+    int pt2 = 3;
+
+//Apresentação das cartas
+
+    printf("Carta número 1\n");
+    printf("Estado: %c\n", Estado);
+    printf("Código da carta: %s\n", codigo_da_carta);
+    printf("Cidade: %s\n", cidade);
+    printf("População Total: %d mil habitantes\n", populacao);
+    printf("Área em Km²: %.3f quilômetros quadrados\n", kmq);
+    printf("PIB: %.1f bilhão de reais\n", pib);
+    printf("Pontos Turísticos: %d pontos maravilhosos\n", pt);
+
+//CARTA DE SÃO FÉLIX DO XINGU
+
+    printf("Carta número 2\n");
+    printf("Estado: %c\n",estado);
+    printf("Código da carta: %s\n", codigodacarta);
+    printf("Cidade: %s\n", cidade2);
+    printf("População Total: %d mil habitantes\n", populacao2);
+    printf("Área em Km²: %.3f quilômetros quadrados\n", kmq2);
+    printf("PIB: %.1f bilhão de reais\n", pib2);
+    printf("Pontos Turísticos: %d pontos maravilhosos\n", pt2);
+    
+    
     return 0;
-}
+}   
